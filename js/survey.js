459fb2bd51222d1fe55209124b443b7c931e6cdd
jeffbartelli <jeff.bartelli@gmail.com>
2020-03-14 21:08:16 -0700

    3/14 harvest.js, ecaCalc.js, html tabs

--- js/survey.js
+++ js/survey.js
@@ -1,18 +1,31 @@
 $(document).ready(function(){
 
-// Need to figure out the validation without reloading the page issue
-window.demographics = function(form) {
-  if (form.checkValidity()) {
-    data.dob = form.dob.value;
-    data.gender = form.gender.value;
-    data.salary = form.salary.value;
-    data.retAge = form.retAge.value;
-    document.getElementById('married').checked ? data.married = 1 : data.married = 0;
-    data.state = form.state.value;
-  } else {alert("Fill the required fields");}
-  console.log(data);
+window.harvest = function(form) {
+  let inputs = document.getElementsByTagName('input');
+  for (let i=0;i<inputs.length;i++) {
+    data[inputs[i].id] = form[inputs[i].id].value;
+    if(inputs[i].type.toLowerCase() == 'checkbox') {
+      if(inputs[i].checked) {
+        data[inputs[i].id] = 1;
+      } else {
+        data[inputs[i].id] = 0;
+      }
+    }
+  }    
+  let dropdowns = document.getElementsByTagName('select');
+  for (let i=0;i<dropdowns.length;i++){
+    data[dropdowns[i].id] = dropdowns[i].value;
+  }
+  // console.log(data);
 }
 
-
-
+window.ecaCalc = function(e) {
+  $(e).addClass('thisCalc');
+  // e.className += " thisCalc";
+  document.querySelector('.ecaCalculatorSalary').parentNode.lastChild.innerHTML = data.salary;
+  document.getElementById(e.parentNode.htmlFor).value = Math.round(
+    data.salary * (e.value/100) > 57000 ? 57000 : data.salary * (e.value/100));
+  $(e).removeClass('thisCalc');
+    // Need a separate calculator or logic for Roth accounts.
+}
 });
