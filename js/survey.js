39c54c5e1f6df8aad28edb5f85f8fd69677ad9e5
jeffbartelli <jeff.bartelli@gmail.com>
2020-03-22 20:33:30 -0700

    3/22 validation complete, harvest

--- js/survey.js
+++ js/survey.js
@@ -37,18 +37,22 @@ window.fixStepIndicator = () => {
 let currentTab = 0;
 window.showTab = (n) => {
   let x = document.getElementsByClassName('tab');
-  x[n].style.display = 'block';
-  if (n == 0) {
-    document.getElementById('prevBtn').style.display = "none";
+  if (!x[n]) {
+    console.log('trigger');
   } else {
-    document.getElementById('prevBtn').style.display = "inline";
+    x[n].style.display = 'block';
+    if (n == 0) {
+      document.getElementById('prevBtn').style.display = "none";
+    } else {
+      document.getElementById('prevBtn').style.display = "inline";
+    }
+    if (n > 0 && n == (Math.max.apply(null,tabOrder))) {
+      document.getElementById('nextBtn').innerHTML = "Submit";
+    } else {
+      document.getElementById('nextBtn').innerHTML = "Next";
+    }
+    fixStepIndicator();
   }
-  if (n > 0 && n == (Math.max.apply(null,tabOrder))) {
-    document.getElementById('nextBtn').innerHTML = "Submit";
-  } else {
-    document.getElementById('nextBtn').innerHTML = "Next";
-  }
-  fixStepIndicator();
 }
 showTab(currentTab);
 $('input:first').select()
@@ -63,50 +67,48 @@ window.nextPrev = (n) => {
   }
   x[currentTab].style.display = 'none';
 
-  if(currentTab >= Math.max.apply(null,tabOrder)) {
-    // harvest();
-    // Need to figure out how to successfully trigger harvest at this point
-  }
   currentTab = tabOrder[tabOrder.indexOf(currentTab)+n];
-  showTab(currentTab);
-  x[currentTab].getElementsByTagName('input')[0].select();
-  window.scrollTo(0,0);
+
+  if (!currentTab) {
+    harvest();
+  // NEED A REDIRECT HERE TO THE NEXT SCREEN
+
+  }
+  if (x[currentTab]) {
+    showTab(currentTab);
+    x[currentTab].getElementsByTagName('input')[0].select();
+    window.scrollTo(0,0);
+  }
+
 }
 
 function validateForm() {
   // This function deals with validation of the form fields
-  let valid = true;
-  let $x = $(".tab");
-  let $y = $x[currentTab].querySelectorAll('.userInput');
-  console.log($y[0].querySelectorAll('input[type=number],input[type=date],select'));
-  let y = $x[currentTab].querySelectorAll('input[type=number],input[type=date],select');
-  // A loop that checks every input field in the current tab:
-  for (let j=0; j<$y.length;j++) {
-    let temp = $y[j].querySelectorAll('input[type=number],input[type=date],select');
-    let said = false;
-    for (let i=0; i<temp.length; i++) {
-      if (temp[i].value != '') {said = true;}
+
+  let x, y, blank, valid = true;
+  x = document.getElementsByClassName("tab");
+  let z = x[currentTab].getElementsByClassName('userInput');
+  for (let j=0; j<z.length; j++) {
+    y = z[j].querySelectorAll('input[type=number],input[type=date],select');
+    for (let i=0; i<y.length; i++) {
+      if (y[i].value != '') {blank = false;}
+
     }
-    if (said == true) {
-      for (let i = 0; i < y.length; i++) {
-        // If a field is required and empty...
-        if (y[i].value == "") {
-          // add an "invalid" class to the field:
-          y[i].className += " invalid";
-          // and set the current valid status to false:
-          valid = false;
+    if (blank == false) {
+      for (let k=0; k<y.length; k++) {
+        if (y[k].value == '') {
+          if(y[k].classList.contains(' invalid')) {
+            continue;
+          } else {
+            y[k].className += " invalid";
+            valid = false;
+          }
         }
       }
-    } else {continue;}
+      blank = true;
+    } 
   }
-  
-  // If the valid status is true, mark the step as finished and valid:
-  if (valid && currentTab > 0) {
-    console.log($(".step")[currentTab]);
-    // NEED TO FIGURE OUT WHY THIS ADDCLASS ISN'T WORKING NOW
-    $(".step")[currentTab].addClass('finish');
-  }
-  return valid; // return the valid status
+  return valid;
 }
 
 window.demographics = (form) => {
@@ -130,9 +132,14 @@ window.ecaCalc = (e) => {
 }
 
 window.harvest = (form) => {
+  // INPUTS DOES NOT INCLUDE SELECT ITEMS. EXPAND DEFINITION TO INCLUDE SELECT, AND TO HANDLE CHECKBOXES CORRECTLY.
   let inputs = $(document.getElementsByTagName('input')).not('.accountTypes').not('.ecaCalculator');
+  for (let i=0; i<inputs.length; i++) {
+    console.log(inputs[i].value);
+  }
   for (let i=0;i<inputs.length;i++) {
-    data[inputs[i].id] = form[inputs[i].id].value;
+    data[inputs[i].id] = inputs[i].value;
+
     if(inputs[i].type.toLowerCase() == 'checkbox') {
       if(inputs[i].checked) {
         data[inputs[i].id] = 1;
@@ -145,6 +152,7 @@ window.harvest = (form) => {
   for (let i=0;i<dropdowns.length;i++){
     data[dropdowns[i].id] = dropdowns[i].value;
   }
+  console.log(inputs);
 }
 
 // STATE DROPDOWN
