9d7551f4088c66bf88140c96f63eedb4aa3ecc0c
jeffbartelli <jeff.bartelli@gmail.com>
2020-04-29 17:01:20 -0700

    4/29 Completed income function consolidation

--- js/assets.js
+++ js/assets.js
@@ -1,10 +1,6 @@
 import {data} from './survey.js';
 import {life, retire, growthRate} from './data.js';
 
-document.addEventListener('DOMContentLoaded', function() {
-
-});
-
 window.calcAge = () => {
   let b = data.demographics.dob.split(/\D/);
   let c = new Date(b[0], --b[1], b[2]);
@@ -27,64 +23,55 @@ window.calcAge = () => {
   };
 }
 
-window.dates = () => {
-  retire.dates = {year: [], age: []};
+window.income = () => {
   let ages = calcAge();
+  retire.dates = {year: [], age: []};
+  retire.totals = {target: [], subtotal: [], taxes: []};
   for (let i=0;i<ages.cycle+1;i++) {
     retire.dates.year.push(ages.currentYr+i);
     retire.dates.age.push(ages.age+i);
+    retire.totals.subtotal.push(0);
   }
-}
-
-window.targetSal = () => {
-  retire.totals = {target: [], subtotal: [], taxes: []};
-  let ages = calcAge();
-  let amount = data.demographics.retSal;
+  let targetSalAmt = data.demographics.retSal;
   for (let i=0;i<ages.cycle+1;i++) {
     if (i < ages.retire - ages.age) {
       retire.totals.target.push(0);
     } else {
-      retire.totals.target.push(amount);
+      retire.totals.target.push(targetSalAmt);
     }
-    amount *= 1+growthRate.inflation 
+    targetSalAmt *= 1+growthRate.inflation 
   };
-}
-
-window.ssi = () => {
+  retire.income = {};
   if(data.ssi.active == true) {
     retire.income.ssi = {annual: []};
-    let ages = calcAge();
-    let fersRate = 0;
-    switch (parseInt(data.ssi.retirementAgeSsi)) { 
-      case 62: fersRate = 0.70; break;
-      case 63: fersRate = 0.76; break;
-      case 64: fersRate = 0.82; break;
-      case 65: fersRate = 0.88; break;
-      case 66: fersRate = 0.94; break;
-      case 67: fersRate = 1.00; break;
-      case 68: fersRate = 1.08; break;
-      case 69: fersRate = 1.16; break;
-      case 70: fersRate = 1.24;
+    let ssiRate = 0;
+    switch (data.ssi.retirementAgeSsi) { 
+      case 62: ssiRate = 0.70; break;
+      case 63: ssiRate = 0.76; break;
+      case 64: ssiRate = 0.82; break;
+      case 65: ssiRate = 0.88; break;
+      case 66: ssiRate = 0.94; break;
+      case 67: ssiRate = 1.00; break;
+      case 68: ssiRate = 1.08; break;
+      case 69: ssiRate = 1.16; break;
+      case 70: ssiRate = 1.24;
     };
-    let benAmount = parseInt(data.ssi.monthlyAmtSsi) * fersRate;
+    let ssiAmount = data.ssi.monthlyAmtSsi * ssiRate;
     for (let i=0; i<ages.cycle+1; i++) {
       if (i < (data.ssi.retirementAgeSsi - ages.age)) {
         retire.income.ssi.annual.push(0);
       } else {
-        retire.income.ssi.annual.push(benAmount);
+        retire.income.ssi.annual.push(ssiAmount);
+        retire.totals.subtotal[i] += ssiAmount;
       }
       if (ages.currentYr + i == 2035) {
-        benAmount *= 0.76;
+        ssiAmount *= 0.76;
       }
-      benAmount *= 1+growthRate.ssi;
+      ssiAmount *= 1+growthRate.ssi;
     };
   };
-}
-
-window.genPension = () => {
   if(data.genPension.active == true) {
     retire.income.genPension = {annual: []};
-    let ages = calcAge();
     let penAmount = parseInt(data.genPension.annualBenAmtGen);
     let penBeginAge = parseInt(data.genPension.benBeginAgeGen);
     let penCola = parseInt(data.genPension.annualColaGen)/100;
@@ -94,21 +81,18 @@ window.genPension = () => {
         retire.income.genPension.annual.push(0);
       } else {
         retire.income.genPension.annual.push(penAmount);
+        retire.totals.subtotal[i] += penAmount;
       }
       if (i >= (penColaAge - ages.age - 1)) {
         penAmount *= 1+penCola;
       }
     }
-  }
-}
-
-window.fersPension = () => {
+  };
   if(data.fersPension.active == true) {
     retire.income.fersPension = {annual: []};
-    let ages = calcAge();
-    let benBeginAge = data.fersPension.benBeginAgeFers;
+    let fersBeginAge = data.fersPension.benBeginAgeFers;
     let fersRate = 0;
-    switch (parseInt(benBeginAge)) { 
+    switch (parseInt(fersBeginAge)) { 
       case 57: fersRate = 0.75; break;
       case 58: fersRate = 0.80; break;
       case 59: fersRate = 0.85; break;
@@ -116,152 +100,115 @@ window.fersPension = () => {
       case 61: fersRate = 0.95; break;
       case 62: fersRate = 1.00; 
     };
-    let benAmount = parseInt(data.fersPension.annualBenAmtFers) * fersRate;
+    let fersAmount = parseInt(data.fersPension.annualBenAmtFers) * fersRate;
     for (let i=0; i<ages.cycle+1; i++) {
-      if (i < benBeginAge - ages.age) {
+      if (i < fersBeginAge - ages.age) {
         retire.income.fersPension.annual.push(0);
       } else {
-        retire.income.fersPension.annual.push(benAmount);
-        benAmount *= 1+growthRate.fers;
+        retire.income.fersPension.annual.push(fersAmount);
+        retire.totals.subtotal[i] += fersAmount;
+        fersAmount *= 1+growthRate.fers;
       }
     }
-  }
-}
-
-window.annuities = () => {
+  };
   if(data.annuities.active == true) {
     retire.income.annuities = {annual: []};
-    let ages = calcAge();
-    let benAmount = parseInt(data.annuities.annualAnnuity);
+    let annAmount = parseInt(data.annuities.annualAnnuity);
     let annuityCola = parseInt(data.annuities.annuityCola)/100;
     let annuityAge = parseInt(data.annuities.annuityAge);
     for (let i=0; i<ages.cycle+1; i++) {
       if (i < annuityAge - ages.age) {
         retire.income.annuities.annual.push(0);
       } else {
-        retire.income.annuities.annual.push(benAmount);
-        benAmount *= 1+annuityCola;
+        retire.income.annuities.annual.push(annAmount);
+        retire.totals.subtotal[i] += annAmount;
+        annAmount *= 1+annuityCola;
       }
     }
-  }
-}
-
-window.vaDisability = () => {
+  };
   if(data.vaDisability.active == true) {
     retire.income.vaDisability = {annual: []};
-    let ages = calcAge();
-    let amount = parseInt(data.vaDisability.annualAmtVa);
+    let vaAmount = parseInt(data.vaDisability.annualAmtVa);
     for (let i=0; i<ages.cycle+1; i++) {
       if (i < ages.retire - ages.age) {
         retire.income.vaDisability.annual.push(0);
       } else {
-        retire.income.vaDisability.annual.push(amount);
+        retire.income.vaDisability.annual.push(vaAmount);
+        retire.totals.subtotal[i] += vaAmount;
       }
-      amount *= 1+growthRate.vaDisability;
+      vaAmount *= 1+growthRate.vaDisability;
     }
-  }
-}
-
-window.ssiDisability = () => {
+  };
   if(data.ssiDisability.active == true) {
     retire.income.ssiDisability = {annual: []};
-    let ages = calcAge();
-    let amount = parseInt(data.ssiDisability.annualAmtSsi);
+    let ssiDisAmount = parseInt(data.ssiDisability.annualAmtSsi);
     for (let i=0; i<ages.cycle+1; i++) {
       if (i < ages.retire - ages.age) {
         retire.income.ssiDisability.annual.push(0);
       } else {
-        retire.income.ssiDisability.annual.push(amount);
+        retire.income.ssiDisability.annual.push(ssiDisAmount);
+        retire.totals.subtotal[i] += ssiDisAmount;
       }
-      amount *=1+growthRate.ssi;
+      ssiDisAmount *=1+growthRate.ssi;
     }
-  }
-}
-
-window.otherDisability = () => {
+  };
   if(data.otherDisability.active == true) {
     retire.income.otherDisability = {annual: []};
-    let ages = calcAge();
-    let amount = parseInt(data.otherDisability.annualAmtOther);
-    let cola = parseInt(data.otherDisability.colaOther)/100;
+    let oDisAmount = parseInt(data.otherDisability.annualAmtOther);
+    let disCola = parseInt(data.otherDisability.colaOther)/100;
     for (let i=0; i<ages.cycle+1; i++) {
       if (i < ages.retire - ages.age) {
         retire.income.otherDisability.annual.push(0);
       } else {
-        retire.income.otherDisability.annual.push(amount);
+        retire.income.otherDisability.annual.push(oDisAmount);
+        retire.totals.subtotal[i] += oDisAmount;
       }
-      amount *=1+cola;
+      oDisAmount *=1+disCola;
     }
-  }
-}
-
-window.retireSal = () => {
+  };
   if(data.retireSal.active == true) {
     retire.income.retireSal = {annual: []};
-    let ages = calcAge();
-    let amount = parseInt(data.retireSal.retSalAmt);
+    let retSalAmount = parseInt(data.retireSal.retSalAmt);
     for (let i=0; i<ages.cycle+1; i++) {
       if (i < data.retireSal.retSalBeginAge - ages.age) {
         retire.income.retireSal.annual.push(0);
       } else if (i < data.retireSal.retSalEndAge - ages.age) {
-        retire.income.retireSal.annual.push(amount);
+        retire.income.retireSal.annual.push(retSalAmount);
+        retire.totals.subtotal[i] += retSalAmount;
       } else {
         retire.income.retireSal.annual.push(0);
       }
-      amount *=1+growthRate.inflation;
+      retSalAmount *=1+growthRate.inflation;
     }
-  }
-}
-
-window.rents = () => {
+  };
   if(data.rents.active == true) {
     retire.income.rents = {annual: []};
-    let ages = calcAge();
-    let amount = parseInt(data.rents.rentalProfits);
+    let rentAmount = parseInt(data.rents.rentalProfits);
     let growth = parseInt(data.rents.rentalProfitsGrowth)/100;
     for (let i=0; i<ages.cycle+1; i++) {
       if (i < ages.retire - ages.age) {
         retire.income.rents.annual.push(0);
       } else {
-        retire.income.rents.annual.push(amount);
+        retire.income.rents.annual.push(rentAmount);
+        retire.totals.subtotal[i] += rentAmount;
       }
-      amount *=1+growth;
+      rentAmount *=1+growth;
     }
-  }
-}
-
-window.otherBen = () => {
+  };
   if(data.otherBen.active == true) {
     retire.income.otherBen= {annual: []};
-    let ages = calcAge();
-    let benAmount = parseInt(data.otherBen.otherBenAmt);
-    let benCola = parseInt(data.otherBen.otherBenCola)/100;
-    let benAge = parseInt(data.otherBen.otherBenBeginAge);
+    let oBenAmount = parseInt(data.otherBen.otherBenAmt);
+    let oBenCola = parseInt(data.otherBen.otherBenCola)/100;
+    let oBenAge = parseInt(data.otherBen.otherBenBeginAge);
     for (let i=0; i<ages.cycle+1; i++) {
-      if (i < benAge - ages.age) {
+      if (i < oBenAge - ages.age) {
         retire.income.otherBen.annual.push(0);
       } else {
-        retire.income.otherBen.annual.push(benAmount);
+        retire.income.otherBen.annual.push(oBenAmount);
+        retire.totals.subtotal[i] += oBenAmount;
       }
-      benAmount *= 1+benCola;
+      oBenAmount *= 1+oBenCola;
     }
-  }
-}
-
-window.tester = () => {
-  calcAge();
-  targetSal();
-  dates();
-  retire.income = {};
-  ssi();
-  genPension();
-  fersPension();
-  annuities();
-  vaDisability();
-  ssiDisability();
-  otherDisability();
-  retireSal();
-  rents();
-  otherBen();
+  };
   console.log(retire);
 }
